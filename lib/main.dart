7831b354639fc8c066ade6ddbb908fa2570824aa
sundong212 <dongsheng.sun@brainco.tech>
2019-12-21 21:13:30 -0500

    finished form course

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/shopping_cart_screen.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -38,6 +40,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           ShoppingCartScreen.routeName: (ctx) => ShoppingCartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
